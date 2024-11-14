ef8785acd1639c463c355855bb46d9b5cc7ab355
Louis | Diox <45403791+LouisHyt@users.noreply.github.com>
2024-11-14 18:44:16 +0100

    update

--- utils/handleSettings.js
+++ utils/handleSettings.js
@@ -55,14 +55,13 @@ const handleSettings = () => {
         const data = await fetch("/assets/jsons/dialogs/settings_callSanta.json")
         const jsonData = await data.json();
         const funFactsUnlocked = JSON.parse(localStorage.getItem("funFactsUnlocked"));
-
         const filteredFunFacts = jsonData.filter(elem => !funFactsUnlocked.includes(elem.id));
+
         let randomFunFact;
         if(!filteredFunFacts.length){
+            randomFunFact = jsonData.find(elem => elem.id === funFactsUnlocked[0]);
             const newFunFactsUnlocked = [funFactsUnlocked[0]];
             localStorage.setItem("funFactsUnlocked", JSON.stringify(newFunFactsUnlocked));
-            randomFunFact = jsonData.find(elem => elem.id === funFactsUnlocked[0]);
-            return
         } else {
             randomFunFact = shuffle(filteredFunFacts)[0];
             funFactsUnlocked.push(randomFunFact.id);
